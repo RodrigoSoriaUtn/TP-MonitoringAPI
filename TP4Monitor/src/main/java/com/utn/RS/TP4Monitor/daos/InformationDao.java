6ab65732b534f0b681161a8402557de095b03bef
Rodrigo Soria <vamosrock@hotmail.com>
2018-04-17 21:22:30 -0300

    Initial classes from maven project

---
+++ TP4Monitor/src/main/java/com/utn/RS/TP4Monitor/daos/InformationDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utn.RS.TP4Monitor.daos;
+
+import com.utn.RS.TP4Monitor.models.Information;
+
+/**
+ *
+ * @author alumno
+ */
+public class InformationDao extends GenericDao <Information, Integer>{
+    
+}
